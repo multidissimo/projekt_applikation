f8b9857117a0a7a4053e72348a1343ef0f534e08
Multi <multidissimo@gmail.com>
2023-11-17 17:16:35 +0100

    Liste und Variablen erstellt, Loop noch nicht fertig

--- Europaquiz.py
+++ Europaquiz.py
@@ -1,75 +1,65 @@
-#Modifizierter und ausgebauter Code auf Basis von tutorialspoint.com
-#Original: https://www.tutorialspoint.com/return-the-input-of-the-entry-widget-in-tkinter
-
 import tkinter as tk
-from tkinter import PhotoImage
-from glob import glob
 from tkinter import *
+from PIL import ImageTk, Image
+from random import randrange
 
-#Nachfolgend wird ein Fenster erzeugt, in welchem eine Eingabe stattfinden kann
-win = Tk()
-win.title("Europaquiz")
-win.geometry("700x250")
+#Liste mit allen Daten (Kürzel, Name, Hauptstadt)
+daten = [["ch","Schweiz","Bern"],["DEU","Deutschland","Berlin"],["BE","Belgien","Brüssel"]]
+
+def start(aktuell, stadt, land):
+    tk.Label(win, text="Hauptstadt von " + land).pack()
+    userInput(aktuell, stadt)
+
+#Zufällige Auswahl
+def auswahl():
+    aktuell = randrange(len(daten))
+    land = daten[aktuell][1]
+    stadt = daten[aktuell][2]
 
 #Textrückgabe des Eingabefelds
-def userInput():
+def userInput(aktuell, stadt):
     p = entry.get()
-    print (p)
+
+    if p == stadt:
+        pasten = Image.open(daten[aktuell][0]+"_g.png").resize((400, 400), Image.LANCZOS)
+    else:
+        pasten = Image.open(daten[aktuell][0]+"_r.png").resize((400, 400), Image.LANCZOS)
+
+    # Erstelle ein neues Bild als Kopie von img_bottom
+    combined_img = pasten.copy()
+
+    # Füge das obere Bild auf das untere Bild ein
+    #combined_img.paste(img_top, (0, 0), img_top)
+
+    # Aktualisiere das Bild im Label
+    img_combined = ImageTk.PhotoImage(combined_img)
+    image_label.config(image=img_combined)
+    image_label.image = img_combined
+
+    print(p)
+
+# Nachfolgend wird ein Fenster erzeugt, in welchem eine Eingabe stattfinden kann
+win = Tk()
+win.title("Europaquiz")
+win.geometry("800x600")
+
+#Initialisiere das Label für das Bild
+img = ImageTk.PhotoImage(Image.open("EuropaleereKarte.png").resize((400, 400), Image.LANCZOS))
+image_label = tk.Label(win, image=img)
+image_label.place(relx=.5, rely=.5, anchor=tk.CENTER)
+
+aktuell = randrange(len(daten))
+land = daten[aktuell][1]
+stadt = daten[aktuell][2]
+
+tk.Label(win, text="Hauptstädtequiz Europa").pack()
+tk.Label(win, text="Hauptstadt von " + land).pack()
 
 #Eingabefeld
-entry = Entry(win, width = 42)
-entry.place(anchor = CENTER)
-entry.pack()
-
-#Feldtitel
-label= Label(win, text="Hauptstädtequiz Europa").pack()
-tk.Label(win, text="Hauptstadt von <>").pack()
+entry = Entry(win, width=30)
+entry.place(relx=.5, rely=.13, anchor=CENTER)
 
 #Button
-tk.Button(win, text= "Absenden", command = userInput).place(relx= .7, rely= .5, anchor= CENTER)
+startButton = tk.Button(win, text="Check", command=userInput(aktuell, stadt)).pack()
 
 win.mainloop()
-
-#Modifizierter und ausgebauter Code auf Basis von @GiovanniPython
-#Original: https://pythonprogramming.altervista.org/how-to-show-and-image-with-tkinter/
-
-#Dieser Teil zeigt die Umrisse der Länder (alphabetisch) in einem neuen Fenster, nachdem eine Eingabe stattfand
-#Die beiden Codes müssen noch verknüpft werden und dienen bis jetzt nur als Basis
-
-class Window:
-    
-    def __init__(self, images: list):
-        self.images = images
-        self.imagepos = 0
-        self.root = tk.Tk()
-        self.widgets()
-        self.root.mainloop()
-
-    def widgets(self):
-        self.lab_image()
-        self.start_button()
-
-    def lab_image(self):
-        self.img = PhotoImage(file=self.images[self.imagepos])
-        self.label = tk.Label(self.root, image=self.img)
-        self.label.pack()
-
-
-
-    def start_button(self):
-        self.butstart = tk.Button(self.root,
-            text="Start",
-            command=self.slideshow)
-        self.butstart.pack()
-
-    def slideshow(self):
-        if self.imagepos < len(self.images) - 1:
-            self.imagepos += 1
-        else:
-            self.imagepos = 0
-        self.img = PhotoImage(file=self.images[self.imagepos])
-        self.label["image"] = self.img
-
-
-images = glob("*png")
-win = Window(images)
